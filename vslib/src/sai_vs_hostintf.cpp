1936d0486405faa451c0989dd4cc79c30b3b6e0d
Kamil Cudnik <kcudnik@microsoft.com>
2017-09-19 09:41:49 -0700

    Add create tap device and set mac address

--- vslib/src/sai_vs_hostintf.cpp
+++ vslib/src/sai_vs_hostintf.cpp
@@ -3,7 +3,18 @@
 
 #include "meta/saiserialize.h"
 
-#define MAX_INTERFACE_NAME_LEN 16
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <sys/ioctl.h>
+#include <net/if_arp.h>
+#include <unistd.h>
+
+#define MAX_INTERFACE_NAME_LEN IFNAMSIZ
 
 sai_status_t vs_recv_hostif_packet(
         _In_ sai_object_id_t hif_id,
@@ -31,6 +42,76 @@ sai_status_t vs_send_hostif_packet(
     SWSS_LOG_ENTER();
 
     return SAI_STATUS_NOT_IMPLEMENTED;
+}
+
+int vs_create_tap_device(const char *dev, int flags)
+{
+    SWSS_LOG_ENTER();
+
+    const char *tundev = "/dev/net/tun";
+
+    int fd = open(tundev, O_RDWR);
+
+    if (fd < 0)
+    {
+        SWSS_LOG_ERROR("failed to open %s", tundev);
+
+        return -1;
+    }
+
+    struct ifreq ifr;
+
+    memset(&ifr, 0, sizeof(ifr));
+
+    ifr.ifr_flags = (short int)flags;  // IFF_TUN or IFF_TAP, IFF_NO_PI
+
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+    int err = ioctl(fd, TUNSETIFF, (void *) &ifr);
+
+    if (err < 0)
+    {
+        SWSS_LOG_ERROR("ioctl TUNSETIFF on fd %d %s failed, err %d", fd, dev, err);
+
+        close(fd);
+
+        return err;
+    }
+
+    return fd;
+}
+
+int vs_set_dev_mac_address(const char *dev, const sai_mac_t mac)
+{
+    SWSS_LOG_ENTER();
+
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (s < 0)
+    {
+        SWSS_LOG_ERROR("failed to create socket, errno: %d", errno);
+
+        return -1;
+    }
+
+    struct ifreq ifr;
+
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+    memcpy(ifr.ifr_hwaddr.sa_data, mac, 6);
+
+    ifr.ifr_hwaddr.sa_family = ARPHRD_ETHER;
+
+    int err = ioctl(s, SIOCSIFHWADDR, &ifr);
+
+    if (err < 0)
+    {
+        SWSS_LOG_ERROR("ioctl SIOCSIFHWADDR on socked %d %s failed, err %d", s, dev, err);
+    }
+
+    close(s);
+
+    return err;
 }
 
 sai_status_t vs_create_hostif_int(
@@ -120,29 +201,46 @@ sai_status_t vs_create_hostif_int(
 
     SWSS_LOG_INFO("creating hostif %s", name.c_str());
 
-    // TODO make this in programatic way since when used without sudo it will prompt
-    // and hang
+    int tapfd = vs_create_tap_device(name.c_str(), IFF_TAP);
 
-    // TODO validate "name" if it's in format EthernetX
-
-    std::string cmd = "/sbin/ip tuntap add name " + name + " mode tap";
-
-    // normally interfaces are in state "DOWN" after create, but we have
-    // configuration in /etc/network/interfaces and they are configured to UP
-
-    int ret = system(cmd.c_str());
-
-    if (ret)
+    if (tapfd < 0)
     {
-        SWSS_LOG_ERROR("creating tap interface %s with command '%s' failed: %d",
-                name.c_str(),
-                cmd.c_str(),
-                ret);
+        SWSS_LOG_ERROR("failed to create TAP device for %s", name.c_str());
 
         return SAI_STATUS_FAILURE;
     }
 
-    // TODO update mac address
+    SWSS_LOG_INFO("created TAP device for %s, fd: %d", name.c_str(), tapfd);
+
+    // XXX currently tapfd is ignored, it should be closed on hostif_remove
+    // and we should use it to read/write packets from that interface
+
+    sai_attribute_t attr;
+
+    attr.id = SAI_SWITCH_ATTR_SRC_MAC_ADDRESS;
+
+    sai_status_t status = vs_generic_get(SAI_OBJECT_TYPE_SWITCH, switch_id, 1, &attr);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_ERROR("failed to get SAI_SWITCH_ATTR_SRC_MAC_ADDRESS on switch %s: %s",
+                sai_serialize_object_id(switch_id).c_str(),
+                sai_serialize_status(status).c_str());
+    }
+
+    int err = vs_set_dev_mac_address(name.c_str(), attr.value.mac);
+
+    if (err < 0)
+    {
+        SWSS_LOG_ERROR("failed to set MAC address %s for %s",
+                sai_serialize_mac(attr.value.mac).c_str(),
+                name.c_str());
+
+        close(tapfd);
+
+        return SAI_STATUS_FAILURE;
+
+    }
 
     // TODO what about FDB entries notifications, they also should
     // be generated if new mac addres will show up on the interface/arp table
