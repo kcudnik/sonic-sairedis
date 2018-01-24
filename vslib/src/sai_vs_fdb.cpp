22398bb6927c8d3368c2eeecb2f964103672f02c
Kamil Cudnik <kcudnik@microsoft.com>
2018-01-24 12:59:05 -0800

    continue

--- vslib/src/sai_vs_fdb.cpp
+++ vslib/src/sai_vs_fdb.cpp
@@ -1,4 +1,5 @@
 #include "sai_vs.h"
+#include "sai_vs_state.h"
 #include "sai_vs_internal.h"
 
 sai_status_t internal_vs_flush_fdb_entries(
@@ -16,6 +17,11 @@ sai_status_t internal_vs_flush_fdb_entries(
      * those 2 sets will be out of sync untill notifications learned/aged will
      * be sent.
      */
+
+    // TODO actually fdb event is not inserted to the local db to objectsMap
+    // and must be inserted !
+    // there maybe conflict sice we insert only based on vlan and mac
+    // and bridge id may differ
 
     // Actually those sets should be in sync in VS except that existing one
     // can contain static entries crated by user, but it will contain all
@@ -38,6 +44,94 @@ sai_status_t internal_vs_flush_fdb_entries(
     //
     // metadata on sairedis needs to support types of consolidated fdb flush events like when vlan is 0 or port is zero
     // flush case needs to be corrected in metadata
+
+    auto &fdbs = g_switch_state_map.at(switch_id)->objectHash.at(SAI_OBJECT_TYPE_FDB_ENTRY);
+
+    // TODO check if attributes exists
+
+    for (auto it = fdbs.begin(); it != fdbs.end();)
+    {
+        if (shouldRemoveFromList(it, attr_count, attr_list);
+        {
+            it = fdbs.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    // todo iterate via all fdbs and crate method shouldRemoveFromList
+    for (uint32_t idx = 0; idx < attr_count; ++idx)
+    {
+        const sai_attribute_t &attr = attr_list[idx];
+
+        switch  (attr.id)
+        {
+            case SAI_FDB_FLUSH_ATTR_BRIDGE_PORT_ID:
+
+                for (auto it = fdbs.begin(); it != fdbs.end();)
+                {
+                    // remove from list all entries not matching bridge port id
+                    if (it->second["SAI_FDB_ENTRY_ATTR_BRIDGE_PORT_ID"]->getAttr()->value.oid != attr.value.oid)
+                    {
+                        it = fdbs.erase(it);
+                    }
+                    else
+                    {
+                        ++it;
+                    }
+                }
+
+                break;
+
+            case SAI_FDB_FLUSH_ATTR_VLAN_ID:
+
+                for (auto it = fdbs.begin(); it != fdbs.end();)
+                {
+                    sai_fdb_entry_t fdb_entry;
+
+                    sai_deserialize_fdb_entry(it->first, fdb_entry);
+
+                    // remove from list all entries not matching vlan id
+                    if (fdb_entry.vlan_id != attr.value.u16)
+                    {
+                        it = fdbs.erase(it);
+                    }
+                    else
+                    {
+                        ++it;
+                    }
+                }
+                break;
+
+            case SAI_FDB_FLUSH_ATTR_ENTRY_TYPE:
+
+                for (auto it = fdbs.begin(); it != fdbs.end();)
+                {
+                    // remove from list all entries not matching type
+                    if (it->second["SAI_FDB_ENTRY_ATTR_TYPE"]->getAttr()->value.s32 != attr.value.s32)
+                    {
+                        it = fdbs.erase(it);
+                    }
+                    else
+                    {
+                        ++it;
+                    }
+                }
+
+                break;
+
+            default:
+                SWSS_LOG_ERROR("attribute %d is not supported yet", attr.id);
+                return SAI_STATUS_NOT_IMPLEMENTED;
+        }
+    }
+
+    // fdbs is now filtered and only contains elements that are ready to be flushed
+
+    std::map<std::string, AttrHash> static_fdbs;
+    std::map<std::string, AttrHash> dynamic_fdbs;
 
     SWSS_LOG_ERROR("not implemented");
 
