8538a2c6c69a6addd3ec3459b5020951126e82f4
kcudnik <kcudnik@gmail.com>
2019-12-10 19:49:31 +0100

    [sairedis] Add sairediscommon.h

---
+++ lib/inc/sairediscommon.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/*
+ * This header will contain definitions used by libsairedis and syncd as well
+ * as libswsscommon (for producer/consumer) and lua scripts.
+ */
+
+// TODO move this to SAI meta repository for auto generate
+
+#define SAI_SWITCH_NOTIFICATION_NAME_BFD_SESSION_STATE_CHANGE   "bfd_session_state_change"
+#define SAI_SWITCH_NOTIFICATION_NAME_FDB_EVENT                  "fdb_event"
+#define SAI_SWITCH_NOTIFICATION_NAME_PACKET_EVENT               "packet_event"
+#define SAI_SWITCH_NOTIFICATION_NAME_PORT_STATE_CHANGE          "port_state_change"
+#define SAI_SWITCH_NOTIFICATION_NAME_QUEUE_PFC_DEADLOCK         "queue_deadlock"
+#define SAI_SWITCH_NOTIFICATION_NAME_SWITCH_SHUTDOWN_REQUEST    "switch_shutdown_request"
+#define SAI_SWITCH_NOTIFICATION_NAME_SWITCH_STATE_CHAENE        "switch_state_change"
+#define SAI_SWITCH_NOTIFICATION_NAME_TAM_EVENT                  "tam_event"
+
